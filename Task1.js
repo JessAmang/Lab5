d5182bd95ff57c1ce7bc774741cb6e97626f6c9a
JessAmang <143070257+JessAmang@users.noreply.github.com>
2024-03-08 21:26:04 +0800

    Task1.js

---
+++ Task1.js
@@ -0,0 +1,61 @@
+let myname = "Jesse Amang";
+
+let myage = 22; 
+
+let isStudent = true; 
+
+
+
+let num1 = 20;
+
+let num2 = 5;
+
+
+
+let sum = num1 + num2;
+
+let difference = num1 - num2;
+
+let product = num1 * num2;
+
+let quotient = num1 / num2;
+
+
+
+if (myage >= 18) {
+
+  console.log(myname + " is an adult.");
+
+} else {
+
+  console.log(myname + " is a minor.");
+
+}
+
+
+
+if (isStudent) {
+
+  console.log(myname + " is a student.");
+
+} else {
+
+  console.log(myname + " is not a student.");
+
+}
+
+
+
+console.log("Sum:", sum);
+
+
+
+console.log("Difference:", difference);
+
+
+
+console.log("Product:", product);
+
+
+
+console.log("Quotient:", quotient);
